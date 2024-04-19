3b976e93f0ad816a9e6a5db09e8af98c7c7366b1
Nivedyakv <nivedyakv2703@gmail.com>
2024-04-19 16:29:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Angularpromise';
+  async showCustomAlert() {
+    try {
+      const result = await Swal.fire({
+        title: 'Custom Alert',
+        html: '<b>This is a custom SweetAlert with Angular!</b>',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel'
+      });
+  
+      if (result.isConfirmed) {
+     
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+      
+      }
+    } catch (error) {
+      console.error('Error showing custom alert:', error);
+    }
+  }
+  
 }
